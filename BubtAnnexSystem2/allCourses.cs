f20faa5f773c26b64f3c51ce510a39a9190116ea
Riad Hossain <riadmahmudeng@gmail.com>
2019-07-05 23:37:49 +0600

    allcourses update

---
+++ BubtAnnexSystem2/allCourses.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubtAnnexSystem2
+{
+    public partial class allCrouses : Form
+    {
+        Presentcrous presentcroues = new allCrouses();
+
+        public PresentCrouses()
+        {
+            InitializeComponent();
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginFrom lform = new LoginFrom();
+            lform.Show();
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            HomeForm hform = new HomeForm();
+            hform.Show();
+        }
+
+        private void Academicinfo_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Academicinfo aform = new Academicinfo();
+            aform.Show();
+        }
+
+        private void PresentCrouses_Load(object sender, EventArgs e)
+        {
+            dataGridViewPresent.DataSource = presentcroues.getPresentcroues();
+
+        }
+    }
+}
